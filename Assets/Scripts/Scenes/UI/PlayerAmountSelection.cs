bbcfdd351ef8a454a4ff56936dc50c9b353427c2
JuanSaz <juan.snchez03@gmail.com>
2025-05-14 12:09:11 -0300

    feat: added new menu buttons and sprites
    - added new sprites for players
    - added ui sprites for player selection
    - fixed buttons when clicking player selection
    - added unsuscribe method to PlayerAmountSelection.cs
    - refactored and organized folders

--- Assets/Scripts/Scenes/UI/PlayerAmountSelection.cs
+++ Assets/Scripts/Scenes/UI/PlayerAmountSelection.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 public class PlayerAmountSelection : MonoBehaviour, IUpdatable
 {
-    private void Awake()
-    {
-        //UpdateManager.Instance.Subscribe(this);
-    }
-
     public void UpdateMe(float deltaTime)
     {
         if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))  //Si presionó el 2 en numpad o normal
@@ -15,7 +10,6 @@ public class PlayerAmountSelection : MonoBehaviour, IUpdatable
 
             UpdateManager.Instance.Unsubscribe(this);
             SceneChanger.Instance.LoadScene("SampleScene");
-            
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))  //Si presionó el 3 en numpad o normal
@@ -24,7 +18,6 @@ public class PlayerAmountSelection : MonoBehaviour, IUpdatable
 
             UpdateManager.Instance.Unsubscribe(this);
             SceneChanger.Instance.LoadScene("SampleScene");
-           
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))   //Si presionó el 4 en numpad o normal
@@ -33,14 +26,16 @@ public class PlayerAmountSelection : MonoBehaviour, IUpdatable
 
             UpdateManager.Instance.Unsubscribe(this);
             SceneChanger.Instance.LoadScene("SampleScene");
-           
         }
-
     }
 
     public void Subscribe()
     {
-        //UpdateManager.Instance.Unsubscribe(this);
         UpdateManager.Instance.Subscribe(this);
+    }
+
+    public void Unsubscribe()
+    {
+        UpdateManager.Instance.Unsubscribe(this);
     }
 }
