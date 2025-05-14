e559ce1e8ca67b3c2bc93de96642c9d8f05b7dde
JuanSaz <juan.snchez03@gmail.com>
2025-05-14 12:30:29 -0300

    fix: back button not working
    - refactored ButtonPlayerSelection.cs

--- Assets/Scripts/Scenes/UI/ButtonPlayerSelection.cs
+++ Assets/Scripts/Scenes/UI/ButtonPlayerSelection.cs
@@ -3,17 +3,10 @@ using UnityEngine;
 public class ButtonPlayerSelection : MonoBehaviour
 {
     [SerializeField] int playerAmount;
-    private PlayerAmountSelection playerAmountSelection;
-
-    private void Awake()
-    {
-        playerAmountSelection = transform.parent.GetComponent<PlayerAmountSelection>();
-    }
 
     public void SelectPlayer()
     {
         GameManager.Instance.SetPlayerAmount(playerAmount);
         SceneChanger.Instance.LoadScene("SampleScene");
-        playerAmountSelection.Unsubscribe();                //Disables the Update method when selecting by clicking
     }
 }
